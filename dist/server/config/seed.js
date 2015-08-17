89cd035d544ac683836701e8f53ea3a230b92f48
Alexandre Martinez <alexandre.martinez@external.grdf.fr>
2015-08-17 18:17:07 +0200

    Rebuild pour deploy

--- dist/server/config/seed.js
+++ dist/server/config/seed.js
@@ -9,70 +9,20 @@ var Product = require('../api/product/product.model');
 var User = require('../api/user/user.model');
 var Interview = require('../api/interview/interview.model');
 
-Interview.find({}).remove(function() {
-  Interview.create({
-      number: 0,
-      sectionOrder: 1,
-      section: 'Origine',
-      title: 'Point de départ',
-      explanation: 'Les *projets* en greenfield et brownfield différent. ![Google](https://www.google.fr/images/nav_logo225.png)',
-      type: 'radio',
-      question: 'Votre produit part-il d\'un existant ?',
-      answers: [{
-        value: 'Oui',
-        next: 1
-      }, {
-        value: 'Non',
-        next: 2
-      }]
-    }, {
-      number: 1,
-      sectionOrder: 2,
-      section: 'Inventaire',
-      title: 'Inventaire de l\'existant',
-      explanation: 'Suivant le type d\'existant, la démarche doit être adaptée.',
-      type: 'choice',
-      question: 'De quels types d\'existants votre produit doit il tenir compte ?',
-      next: 2,
-      answers: [{
-        value: 'Application existante',
-        next: 3
-      }, {
-        value: 'Classeur Excel pour un calculateur',
-        next: 4
-      }]
-    }, {
-      number: 2,
-      sectionOrder: 3,
-      section: 'Solution',
-      title: 'Focus sur la solution',
-      explanation: 'La question miracle',
-      type: 'text',
-      question: 'Maintenant je voudrai vous poser une question bizarre. Imaginez que pendant que vous dormez la nuit prochaine et que toute la maison est calme, un miracle se produit. Le miracle consiste en ce que le problème qui vous a amené ici est résolu. Cependant, comme vous êtes endormi, vous ne savez pas que le miracle est arrivé. Alors, quand vous vous réveillez demain matin, qu’est-ce qui sera différent qui vous dira qu’un miracle a eu lieu et que le problème qui vous a amené ici est résolu ?',
-      answers: {}
-    }, {
-      number: 3,
-      sectionOrder: 2,
-      section: 'Inventaire',
-      title: 'Application existante',
-      explanation: 'Lors de la reprise d\'une application existante ...',
-      type: 'information',
-      question: 'Maintenant je voudrai vous poser une question bizarre. Imaginez que pendant que vous dormez la nuit prochaine et que toute la maison est calme, un miracle se produit. Le miracle consiste en ce que le problème qui vous a amené ici est résolu. Cependant, comme vous êtes endormi, vous ne savez pas que le miracle est arrivé. Alors, quand vous vous réveillez demain matin, qu’est-ce qui sera différent qui vous dira qu’un miracle a eu lieu et que le problème qui vous a amené ici est résolu ?',
-      answers: {}
-    }, {
-      number: 4,
-      sectionOrder: 2,
-      section: 'Inventaire',
-      title: 'Classeur Excel',
-      explanation: 'Classeur Excel, problèmes de transcription',
-      type: 'information',
-      question: 'Maintenant je voudrai vous poser une question bizarre. Imaginez que pendant que vous dormez la nuit prochaine et que toute la maison est calme, un miracle se produit. Le miracle consiste en ce que le problème qui vous a amené ici est résolu. Cependant, comme vous êtes endormi, vous ne savez pas que le miracle est arrivé. Alors, quand vous vous réveillez demain matin, qu’est-ce qui sera différent qui vous dira qu’un miracle a eu lieu et que le problème qui vous a amené ici est résolu ?',
-      answers: {}
-    },
-    function() {
-      console.log('finish populating interview questions');
-    })
-});
+var YAML = require('js-yaml');
+var fs = require('fs');
+var itwContent = fs.readFileSync(__dirname + '/questions.yml', 'utf8');
+var productInterview = YAML.safeLoadAll(
+  itwContent,
+  function(productInterview) {
+    console.log(productInterview.questions);
+    Interview.find({}).remove(function() {
+      Interview.create(productInterview.questions,
+        function() {
+          console.log('finish populating interview questions');
+        })
+    });
+  });
 
 User.find({}).remove(function() {
   User.create({
